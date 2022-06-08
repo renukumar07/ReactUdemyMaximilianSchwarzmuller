0fee43e7ea852b3995f26a7cfee02ebcdbe227ac
renukumar07 <renukumar90@gmail.com>
2022-06-08 23:33:16 +0530

    Assignment 4: Time to Practice: Conditional Content

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
