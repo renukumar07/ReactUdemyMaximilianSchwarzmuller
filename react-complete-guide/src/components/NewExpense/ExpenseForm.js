1f44a8ebc40ecb5da864416e6cd24ae6bae5fecd
renukumar07 <renukumar90@gmail.com>
2022-06-08 13:38:15 +0530

    57. Handling Form Submission

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -33,8 +33,19 @@ const ExpenseForm = () => {
     //   enteredDate: event.target.value,
     // });
   };
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
